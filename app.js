32a8c20ee64f9e91265dc4a239aa81b1937176d1
Teodor Todorov <teodortodorov1710@gmail.com>
2022-06-08 10:39:42 +0200

    update2

--- app.js
+++ app.js
@@ -19,16 +19,19 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
+    var btn = document.getElementsByClassName("btn");
   var dots = document.getElementsByClassName("demo");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
+     if (i < btn.length) {btn[i].style.display = "none";}
   }
   for (i = 0; i < dots.length; i++) {
      dots[i].className = dots[i].className.replace(" w3-white", "");
   }
   x[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " w3-white";
-
+  btn[slideIndex-1].style.display = "block";
+  console.log (btn)
 }
